e2d18caeb0fcd2f00a9cb463fb9da06014eae727
Demon Singur <demonsingur@gmail.com>
2018-05-30 15:21:34 +0000

    Revert "msm8998-common: Update to oreo NXP NFC"
    This reverts commit b2c6895d13fa23efafbe633cf6a21eb84fe8b3de.
    
    Change-Id: I40d30acc726f40e0a7590edf3653e955f0fcc993

--- msm8998.mk
+++ msm8998.mk
@@ -278,12 +278,8 @@ PRODUCT_PACKAGES += \
 # NFC
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
-    com.gsma.services.nfc \
     com.nxp.nfc.nq \
-    com.nxp.nfc.nq.xml \
-    libnqnfc-nci \
     nfc_nci.nqx.default \
-    libnqp61-jcop-kit \
     NQNfcNci \
     nqnfcee_access.xml \
     nqnfcse_access.xml \
@@ -291,8 +287,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.0-impl \
-    vendor.nxp.hardware.nfc@1.0-impl \
-    vendor.nxp.hardware.nfc@1.0-service
+    android.hardware.nfc@1.0-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf
