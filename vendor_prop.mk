1ce695f9501df4183f18edb293d03cd7a53872f1
barracuda7 <barracudaking@gmail.com>
2018-07-16 10:14:33 +1000

    add 60 fps

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -65,7 +65,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.xm.green.r=0.97 \
     persist.dualcam.lpm.enable=1 \
     vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.miui.cit,org.lineageos.snap \
-    vidc.enc.dcvs.extra-buff-count=2
+    vidc.enc.dcvs.extra-buff-count=2 \
+    persist.camera.eis.enable=1 \
+    persist.camera.is_mode=4 \
+    debug.hwui.renderer=skiagl \
+    persist.camera.thermal.mode=frameskip \
+    encoder.video.profile=high
 
 # CNE
 PRODUCT_PROPERTY_OVERRIDES += \
